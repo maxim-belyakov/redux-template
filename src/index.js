933b069042f6bd09a59684c86496beca53404133
Maxim Belyakov <maxim.a.belyakov+g@gmail.com>
2019-12-16 23:29:25 +0200

    create rootReducer

--- src/index.js
+++ src/index.js
@@ -6,8 +6,9 @@ import * as serviceWorker from './serviceWorker';
 
 import { Provider } from "react-redux";
 import { createStore } from 'redux';
+import rootReducer from "./reducers/index";
 
-const store = createStore(() => { });
+const store = createStore(rootReducer);
 
 ReactDOM.render(<Provider store={store}> <App /> </Provider>, document.getElementById('root'));
 
